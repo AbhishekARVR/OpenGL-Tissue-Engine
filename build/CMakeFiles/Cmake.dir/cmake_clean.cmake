eee35ddfe25f839c47ff4defe3d45761d0ae2685
AbhishekARVR <ashindegz@gmail.com>
2025-09-25 00:47:10 +0530

    Cmake done

---
+++ build/CMakeFiles/Cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cmake.dir/main.cpp.o"
+  "CMakeFiles/Cmake.dir/main.cpp.o.d"
+  "Cmake"
+  "Cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/glad_obj.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glad_obj.dir/external/glad/glad.c.o"
+  "CMakeFiles/glad_obj.dir/external/glad/glad.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glad_obj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
